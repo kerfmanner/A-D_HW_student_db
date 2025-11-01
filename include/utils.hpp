d4b097ebead80c8b3a697c8c35a7ba17e3553dea
luka <konovluka@gmail.com>
2025-11-01 23:59:37 +0200

    birthday_key and write_to_csv functions

--- include/utils.hpp
+++ include/utils.hpp
@@ -11,5 +11,7 @@
 std::vector<std::string> split_line(std::istringstream &str, const std::string &delim);
 Student get_student_from_line(std::istringstream &str);
 std::vector<Student> read_students_from_csv(const std::string& path);
+void write_students_to_csv(const std::vector<Student> &students, const std::string& path);
+int birthday_key(int month, int day);
 
 #endif //HMW_1_UTILS_HPP
--- src/utils.cpp
+++ src/utils.cpp
@@ -7,6 +7,10 @@
 #include <iostream>
 #include <sstream>
 
+int birthday_key(const int month, const int day) {
+    return (month << 5) | day;   // month*32 + day
+}
+
 std::vector<std::string> split_line(std::istringstream &str, const char
     &delim) {
     std::vector<std::string> tokens;
@@ -62,4 +66,32 @@ std::vector<Student> read_students_from_csv(const std::string &path) {
     }
 
     return students;
+}
+
+
+void write_students_to_csv(const std::vector<Student> &students,
+                           const std::string& path)
+{
+    std::ofstream file(path);
+    if (!file.is_open()) {
+        throw std::runtime_error("Could not open file for writing: " + path);
+    }
+
+    // header
+    file << "m_name,m_surname,m_email,m_birth_year,m_birth_month,m_birth_day,m_group,m_rating,m_phone_number\n";
+
+    for (const auto& s : students) {
+        file << s.m_name << ","
+             << s.m_surname << ","
+             << s.m_email << ","
+             << s.m_birth_year << ","
+             << s.m_birth_month << ","
+             << s.m_birth_day << ","
+             << s.m_group << ","
+             << s.m_rating << ","
+             << s.m_phone_number
+             << "\n";
+    }
+
+    file.close();
 }
