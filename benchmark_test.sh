3645ab6a87a64e477cd65d89141e51dfd6b388a5
kerfmanner <konovluka@gmail.com>
2025-11-02 14:24:25 +0200

    fixed bash for benchmark tests

--- benchmark_test.sh
+++ benchmark_test.sh
@@ -4,15 +4,15 @@ set -e
 echo "db,input_size,ops" > ops.csv
 echo "db,input_size,peak_kb" > memory.csv
 
-for db in 1 1L 2 2L 3; do
+for db in 1 1L 1S 2 2L 2S 3; do
   for n in 100 1000 10000 100000; do
     echo "*** DB=$db  N=$n ***"
 
     # time benchmark
-    ./benchmark --db=$db --input=${n}.csv --mode=time
+    ./build/benchmark --db=$db --input=./dataset/${n}.csv --mode=time
 
     # memory benchmark
-    /usr/bin/time -v ./benchmark --db=$db --input=${n}.csv --mode=memory 2>/dev/null
+    /usr/bin/time -v ./build/benchmark --db=$db --input=./dataset/${n}.csv --mode=memory 2>/dev/null
     # 2>/dev/null hides stdout of time
   done
 done
